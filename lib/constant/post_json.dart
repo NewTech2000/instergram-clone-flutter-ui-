ea652484c227e1c82f3c149f472a689263b89c6c
Your Name <you@example.com>
2022-01-14 15:01:45 +0530

    upload project

---
+++ lib/constant/post_json.dart
@@ -0,0 +1,56 @@
+List posts = [
+  {
+    "id": 1,
+    "name": "panhchaneath_ung",
+    "profileImg":
+        "https://scontent.fcmb4-2.fna.fbcdn.net/v/t1.6435-9/145765621_253705879528911_1159292961773556037_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=YNj5nC5azBcAX-vExos&_nc_ht=scontent.fcmb4-2.fna&oh=00_AT9Y6tisobUCLKeVMeAPGQxIqTiAIKXRGqsymepbL8FSBg&oe=61E7A508",
+    "postImg":
+        "https://scontent.fcmb4-2.fna.fbcdn.net/v/t39.30808-6/p843x403/269746242_449327859966711_5182211027482894652_n.jpg?_nc_cat=110&_nc_rgb565=1&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=S328IexHAFMAX8j4LlN&_nc_ht=scontent.fcmb4-2.fna&oh=00_AT8VRYrgdI4LG1x4RhUayr0elKOvbWWTzYyiepcxJEcyNw&oe=61C71866",
+    "caption": " SERIOUS MODE during SERIOUS RAINY DAY",
+    "isLoved": true,
+    "commentCount": "10",
+    "likedBy": "whereavygoes",
+    "timeAgo": "1 day ago"
+  },
+  {
+    "id": 2,
+    "name": "whereavygoes",
+    "profileImg":
+        "https://scontent.fcmb4-2.fna.fbcdn.net/v/t1.6435-1/p240x240/65387113_1028879574168572_7787436707409821696_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=7206a8&_nc_ohc=ufWQL8y-8lcAX8csX2G&tn=gbHntln2F_D6c732&_nc_ht=scontent.fcmb4-2.fna&oh=00_AT9KWFUm_LEy1GJsrNo6gmX1Lu50YqozUc-5627-GrXkSA&oe=61E8002A",
+    "postImg":
+        "https://scontent.fcmb4-2.fna.fbcdn.net/v/t39.30808-6/p843x403/243586565_1724591197930736_7821985323835467126_n.jpg?_nc_cat=108&_nc_rgb565=1&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=wGR5ip7d_fgAX9qVsPG&_nc_ht=scontent.fcmb4-2.fna&oh=00_AT8a52iykKokOXbgu7O44Qb2BDZ4Cv5U55WFS7fAMdcgZA&oe=61C8D574",
+    "caption":
+        " ទឹកមុខនៅពេលយើងអត់ចង់ថតរូបនៅកន្លែងសួនផ្កា ហើយគេបង្ខំអោយយើងថត។ 😏😒",
+    "isLoved": true,
+    "commentCount": "10",
+    "likedBy": "sonitakhoun",
+    "timeAgo": "1 day ago"
+  },
+  {
+    "id": 3,
+    "name": "allef_vinicius",
+    "profileImg":
+        "https://scontent.fcmb4-2.fna.fbcdn.net/v/t1.6435-1/133873705_102173978499454_1377959414142840133_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=7206a8&_nc_ohc=c7kr0qTzQuQAX8ilMLw&_nc_ht=scontent.fcmb4-2.fna&oh=00_AT__bcWxnzW_vExQPxVkj38GYV_CigPNh1ns36O4whjIOA&oe=61EA2F43",
+    "postImg":
+        "https://scontent.fcmb4-2.fna.fbcdn.net/v/t1.6435-9/133910583_102346648482187_1326165523218157225_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=-1Q4BQutKQgAX8Ve4Se&_nc_ht=scontent.fcmb4-2.fna&oh=00_AT_OxZtDCir3Ck_cfvti7Pko521T1T9vFRjN85AuvG4D5A&oe=61E768A7",
+    "caption": " It is a good to day",
+    "isLoved": false,
+    "commentCount": "60",
+    "likedBy": "babysweetiepie",
+    "timeAgo": "3 day ago"
+  },
+  {
+    "id": 4,
+    "name": "babysweetiepie",
+    "profileImg":
+        "https://scontent.fcmb4-2.fna.fbcdn.net/v/t31.18172-8/22339226_10155205750328049_695849665160713547_o.jpg?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=kjvsHpPV5S4AX_zVTgT&_nc_ht=scontent.fcmb4-2.fna&oh=00_AT-NZrV0TOuOX24HJ6TvMREC4ySnKTDcIgI9SpiBB-pcJA&oe=61E8C8D4",
+    "postImg":
+        "https://scontent.fcmb4-2.fna.fbcdn.net/v/t39.30808-6/p843x403/269767457_4601111649944703_1156226508038848776_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=730e14&_nc_ohc=yTWE-shqZv0AX_uAKHE&_nc_ht=scontent.fcmb4-2.fna&oh=00_AT_YK-UBitAG-Pf6xd3fDby-40Y7-U16044yl2FnE5ijGQ&oe=61C870C8",
+    "caption":
+        "I love how a 6-year-old kid took better photos than many of my friends out there 😂",
+    "isLoved": false,
+    "commentCount": "70",
+    "likedBy": "sonitakhoun",
+    "timeAgo": "3 day ago"
+  }
+];
---
+++ lib/constant/story_json.dart
@@ -0,0 +1,38 @@
+// user prfile
+String profile =
+    "https://scontent.fcmb4-2.fna.fbcdn.net/v/t1.6435-1/p240x240/65387113_1028879574168572_7787436707409821696_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=7206a8&_nc_ohc=ufWQL8y-8lcAX8csX2G&tn=gbHntln2F_D6c732&_nc_ht=scontent.fcmb4-2.fna&oh=00_AT9KWFUm_LEy1GJsrNo6gmX1Lu50YqozUc-5627-GrXkSA&oe=61E8002A";
+String name = "panhchaneath_ung";
+
+// stories
+List stories = [
+  {
+    "id": 1,
+    "img":
+        "https://scontent.fcmb4-2.fna.fbcdn.net/v/t39.30808-6/261024958_444191010597170_605594056764442729_n.jpg?_nc_cat=104&_nc_rgb565=1&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=h7ovJue0rwAAX8oDp3z&_nc_ht=scontent.fcmb4-2.fna&oh=00_AT_bGlW41C_W0YSLt_ajYlFA7POHVg-QU28foNrQ_EHnhg&oe=61C9083F",
+    "name": "sonitakhoun"
+  },
+  {
+    "id": 2,
+    "img":
+        "https://scontent.fcmb4-2.fna.fbcdn.net/v/t39.30808-1/s320x320/263505863_514298853389526_4965558091265493421_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=c6021c&_nc_ohc=RhnC8cwFhIMAX8RHkCU&_nc_oc=AQly-HxACxIXZChPtGHcRImMbu6gzE78ofLDAJSDhkYVx6-XpCBT47j-NBZkl33PfGs&_nc_ht=scontent.fcmb4-2.fna&oh=00_AT9BrCbFAGKqI4j-9iwl5azNtWfHgnn4xaIsVkNxtRV_3w&oe=61C7D89E",
+    "name": "babysweetiepie"
+  },
+  {
+    "id": 3,
+    "img":
+        "https://scontent.fcmb4-2.fna.fbcdn.net/v/t1.6435-9/196638726_117469407228273_2567129077629322073_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=usl3Qbk2EdgAX8Zb3l8&_nc_ht=scontent.fcmb4-2.fna&oh=00_AT_g_1f4-2660f2bFbRLIoTWCpygvUlo8jVVZ5G8J5aE1A&oe=61EAA058",
+    "name": "whereavygoes"
+  },
+  {
+    "id": 4,
+    "img":
+        "https://images.unsplash.com/photo-1525879000488-bff3b1c387cf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    "name": "collins_lesulie"
+  },
+  {
+    "id": 5,
+    "img":
+        "https://images.unsplash.com/photo-1517070208541-6ddc4d3efbcb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    "name": "tyler_nix"
+  }
+];
